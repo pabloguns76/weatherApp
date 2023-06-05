def48384606a3c1df1e0fa6ebbddd8eddeddd761
Matias <pablomonsalvo29@gmail.com>
2023-06-05 00:18:17 -0300

    app con estilos

--- src/app/components/weather-app/weather-app.component.ts
+++ src/app/components/weather-app/weather-app.component.ts
@@ -38,8 +38,40 @@ export class WeatherAppComponent implements OnInit {
     this.weatherService.getCurrentWeather(query).subscribe( (data: any) => {
       this.weatherCurrent = data;
       console.log( this.weatherCurrent );
-      
+
     });
 
 }
+
+isStartOfDay(weather: weatherData): boolean {
+  const forecastDate = new Date(weather.dt_txt).getHours();
+  return forecastDate === 0; // Verifica si es la primera hora del día (00:00)
+}
+
+
+
+getMaxTemperatureOfDay(weather: weatherData): number {
+  const forecastDate = new Date(weather.dt_txt).getDate();
+  const temperatures = this.weatherForecast
+    .filter(forecast => new Date(forecast.dt_txt).getDate() === forecastDate)
+    .map(forecast => forecast.main.temp_max);
+  return Math.max(...temperatures);
+}
+
+getMinTemperatureOfDay(weather: weatherData): number {
+  const forecastDate = new Date(weather.dt_txt).getDate();
+  const temperatures = this.weatherForecast
+    .filter(forecast => new Date(forecast.dt_txt).getDate() === forecastDate)
+    .map(forecast => forecast.main.temp_min);
+  return Math.min(...temperatures);
+}
+getDayName(dateString: string): string {
+  const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  const date = new Date(dateString);
+  const dayIndex = date.getDay();
+  return days[dayIndex];
+}
+
+
+
 }
